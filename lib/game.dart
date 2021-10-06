00fea7f9ad2cf161d7d18fa7f14454b40f2d3092
ImranIqbalS <imranshaikh5122@gmail.com>
2021-10-06 14:29:00 +0530

    snake movement created

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:snake_game/direction.dart';
 import 'package:snake_game/piece.dart';
 
 class GamePage extends StatefulWidget {
@@ -14,8 +16,28 @@ class _GamePageState extends State<GamePage> {
   late int upperBoundX, upperBoundY, lowerBoundX, lowerBoundY;
   late double screenWidth, screenHeight;
   int step = 20;
-  // int length = 5;
+  int length = 5;
   List<Offset> positions = [];
+  Direction direction = Direction.right;
+  late Timer timer;
+  void changeSpeed() {
+    if (timer != null && timer.isActive) {
+      timer.cancel();
+    }
+    timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
+      setState(() {});
+    });
+  }
+
+  void restart() {
+    changeSpeed();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    restart();
+  }
 
   int getNearestTens(int num) {
     int output;
@@ -39,20 +61,39 @@ class _GamePageState extends State<GamePage> {
     if (positions.length == 0) {
       positions.add(getRandomPosition());
     }
-    // while (length > positions.length) {
-    //   positions.add(positions[positions.length - 1]);
-    // }
+    while (length > positions.length) {
+      positions.add(positions[positions.length - 1]);
+    }
+    for (var i = positions.length - 1; i > 0; i--) {
+      positions[i] = positions[i - 1];
+    }
+    positions[0] = getNextPosition(positions[0]);
+  }
+
+  Offset getNextPosition(Offset position) {
+    Offset nextPosition;
+    if (direction == Direction.right) {
+      nextPosition = Offset(position.dx + step, position.dy);
+    } else if (direction == Direction.left) {
+      nextPosition = Offset(position.dx - step, position.dy);
+    } else if (direction == Direction.up) {
+      nextPosition = Offset(position.dx, position.dy - step);
+    } else if (direction == Direction.down) {
+      nextPosition = Offset(position.dx, position.dy + step);
+    }
   }
 
   List<Piece> getPieces() {
     final pieces = <Piece>[];
     draw();
-    pieces.add(Piece(
-      color: Colors.red,
-      size: step,
-      posX: positions[0].dx.toInt(),
-      posY: positions[0].dy.toInt(),
-    ));
+    for (var i = 0; i < length; ++i) {
+      pieces.add(Piece(
+        color: Colors.red,
+        size: step,
+        posX: positions[0].dx.toInt(),
+        posY: positions[0].dy.toInt(),
+      ));
+    }
     return pieces;
   }
 
